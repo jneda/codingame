8432db706713d70d14d6b00e127f3e1992481242
Julien Dané <julien.Dane@gmail.com>
2023-12-14 17:13:02 +0100

    Move source files into src folder

--- hello.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "@jest/globals";
-import { sayHello } from "./hello";
-
-describe("sayHello", () => {
-  test("says Hello!", () => {
-    expect(sayHello()).toBe("Hello!");
-  });
-});
